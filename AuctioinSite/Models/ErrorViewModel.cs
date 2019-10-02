fad014171d60f2c146391da05d995755ecf55e1a
steven ballard <stevenballardstudios@gmail.com>
2019-10-02 14:14:34 -0700

    Add project files.

---
+++ AuctioinSite/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace AuctioinSite.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
