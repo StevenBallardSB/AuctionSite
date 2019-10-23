04b83bc8d376d51ba71d3e1732fcd6c3dcaba1fb
steven ballard <stevenballardstudios@gmail.com>
2019-10-23 14:43:05 -0700

    Added unit test for returning the index

---
+++ AuctioinSiteTests/Controllers/HomeControllerTests.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using AuctioinSite.Controllers;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AuctioinSite.Controllers.Tests
+{
+    [TestClass()]
+    public class HomeControllerTests
+    {
+        [TestMethod()]
+        public void Index_ReturnsViewResult()
+        {
+            HomeController home = new HomeController();
+
+            var result = home.Index();
+
+            Assert.IsNotNull(result);
+        }
+    }
+}
