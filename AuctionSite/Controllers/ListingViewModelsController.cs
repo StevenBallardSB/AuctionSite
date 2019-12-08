9955b50f15ce04c45cbb30d58e931d4342e8c626
Steven Ballard <stevenballardstudios@gmail.com>
2019-12-07 21:49:40 -0800

    Added listing Author to the bind

--- AuctionSite/Controllers/ListingViewModelsController.cs
+++ AuctionSite/Controllers/ListingViewModelsController.cs
@@ -54,7 +54,7 @@ namespace AuctionSite.Controllers
         // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Create([Bind("listingId,listingName,listItemCondition,isShipping,listingBuyOutPrice,lisingStartingPrice,listingPostDate,listingEndDate,listingDescription,listingCategory,listingImageURL")] ListingViewModel listingViewModel)
+        public async Task<IActionResult> Create([Bind("listingId,listingName,listingAuthor,listItemCondition,isShipping,listingBuyOutPrice,lisingStartingPrice,listingPostDate,listingEndDate,listingDescription,listingCategory,listingImageURL")] ListingViewModel listingViewModel)
         {
             if (ModelState.IsValid)
             {
