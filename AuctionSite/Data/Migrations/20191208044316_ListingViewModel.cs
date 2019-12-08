b8c9bd9b895d127ce0b41fcdcaba8b7609332349
Steven Ballard <stevenballardstudios@gmail.com>
2019-12-07 21:27:43 -0800

    Added code to show the author name, I still don't know how to set it yet.

---
+++ AuctionSite/Data/Migrations/20191208044316_ListingViewModel.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace AuctioinSite.Data.Migrations
+{
+    public partial class ListingViewModel : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "listingAuthor",
+                table: "ListingViewModel",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "listingAuthor",
+                table: "ListingViewModel");
+        }
+    }
+}
